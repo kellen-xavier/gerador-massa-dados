71a1e80a1e2abff1d1c126297fa12c3618158a8f
kellen xavier <kellenxaviier@gmail.com>
2025-01-25 21:10:04 -0300

    feat: gerador de cpf valido

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use std::char::from_digit;
+use fake::faker::name::en::Name;
+use fake::Fake;
+use rand::{thread_rng, Rng};
+
+fn gerar_nome() -> String {
+    Name().fake()
+}
+
+fn gerar_cpf() -> String {
+    let mut cpf: Vec<u8> = (0..9).map(|_| thread_rng().gen_range(0..=9) as u8).collect();
+
+    let sum1: u32 = cpf.iter()
+        .enumerate()
+        .map(|(i, &digit)| digit as u32 * (10 - i as u32))
+        .sum();
+
+    let dv1 = (sum1 * 10 % 11) % 10;
+        cpf.push(dv1 as u8);
+
+    let sum2: u32 = cpf.iter()
+        .enumerate()
+        .map(|(i, &digit)| digit as u32 * (10 - i as u32))
+        .sum();
+    let dv1 = (sum1 * 10 % 11) % 10;
+    cpf.push(dv1 as u8);
+
+    cpf.iter()
+        .enumerate()
+        .map(|(i, &digit)| {
+            if i == 3 || i == 6 {
+                format!(".{}", digit)
+            } else if i == 9 {
+                format!("-{}", digit)
+            } else {
+                digit.to_string()
+            }
+        })
+        .collect::<String>()
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let nome = gerar_nome();
+    println!("Nome gerado: {}", nome);
+
+    let cpf = gerar_cpf();
+    println!("CPF gerado: {}", cpf);
 }
